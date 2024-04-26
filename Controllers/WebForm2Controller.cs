ef0657a8ac15e29eb524e8ad65eb9eefe6fac190
AMINE nahed <aminenahed19@gmail,com>
2024-04-26 17:26:20 +0200

    MAJ SUR V2

---
+++ Controllers/WebForm2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atelier2.Controllers
+{
+    public class WebForm2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
