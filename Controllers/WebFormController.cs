0a0d969f9e02908814fe40f7db0770943517251f
AMINE nahed <aminenahed19@gmail,com>
2024-04-26 17:23:04 +0200

    Ajout Page Client

---
+++ Controllers/WebFormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atelier2.Controllers
+{
+    public class WebFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
